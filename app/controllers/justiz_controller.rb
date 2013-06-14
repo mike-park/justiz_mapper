3ba996f754dcf072584cd783a32979394b7b8e75
Mike Park <mikep@quake.net>
2013-06-14 17:22:29 +0200

    dont do types individually -- it times out

--- app/controllers/justiz_controller.rb
+++ app/controllers/justiz_controller.rb
@@ -6,16 +6,20 @@ class JustizController < InheritedResources::Base
     states = scraper.states
     types = scraper.court_types
     count = 0
+    new_records = []
     states.keys.each do |state|
-      types.keys.each do |type|
-        next if type == 'ALL'
-        scraper.scrape(type, state).each do |contact|
-          hash = {}.merge contact
-          Court.create!(state: states[state], category: types[type], name: hash[:court], blob: {justiz: hash})
-          count += 1
-        end
+      next if state == 'ALL'
+      type = 'ALL'
+      puts "Scraping #{state} #{type}"
+      scraper.scrape(type, state).uniq.each do |contact|
+        hash = {}.merge contact
+        new_records.push({state: states[state], name: hash[:court], blob: {justiz: hash}})
+        count += 1
       end
-      break if count > 10
+    end
+    puts "Saving records #{count}"
+    new_records.each do |record|
+      Court.create!(record)
     end
     redirect_to courts_path, notice: "#{count} entries downloaded"
   end
