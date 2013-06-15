9e52cb86e9674b49eb162aa4b91f91d19d2c5107
mikep@quake.net <>
2013-06-15 13:42:16 +0000

    fix refactoring.
    harder than it looks to write live code on codenvy after being use to RubyMine

--- app/controllers/justiz_controller.rb
+++ app/controllers/justiz_controller.rb
@@ -2,25 +2,23 @@ class JustizController < InheritedResources::Base
   def create
     Court.delete_all
 
-    scraper = Justiz::Scraper::Courts.new
-    states = scraper.states
-    types = scraper.court_types
-    count = 0
     new_records = scrape('ALL', 'NRW')
     puts "Saving records #{new_records.length}"
     new_records.each do |record|
       Court.create!(record)
     end
-    redirect_to courts_path, notice: "#{count} entries downloaded"
+    redirect_to courts_path, notice: "#{new_records.length} entries downloaded"
   end
   
   private
   
   def scrape(category, state)
-      puts "Scraping #{state} #{category}"
-      scraper.scrape(category, state).uniq.map do |contact|
-        hash = {}.merge contact
-        {state: states[state], name: hash[:court], blob: {justiz: hash}}
-      end    
+    scraper = Justiz::Scraper::Courts.new
+    states = scraper.states
+    puts "Scraping #{state} #{category}"
+    scraper.scrape(category, state).uniq.map do |contact|
+      hash = {}.merge contact
+      {state: states[state], name: hash[:court], blob: {justiz: hash}}
+    end    
   end
 end
